357264ce756d9c64b735178e04271eead219a87f
Brindha Sambath <brindha.sambath@esri.in>
2020-04-19 17:03:43 +0530

    resolve bug

--- src/redux/cart/cart.redcuer.js
+++ src/redux/cart/cart.redcuer.js
@@ -21,7 +21,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.REMOVE_ITEM:
       return {
         ...state,
-        cartItems: removeItemFromCart(state.cartItem, action.payload),
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
